569252606a1149bc0ddc54992f6ee1715aed1851
russianroadman <realgar2@gmail.com>
2022-03-29 23:47:27 +0800

    Initial commit

---
+++ src/main/java/com/tanto/roadover/RoadoverApplication.java
@@ -0,0 +1,13 @@
+package com.tanto.roadover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanto/roadover/RoadoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanto.roadover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
